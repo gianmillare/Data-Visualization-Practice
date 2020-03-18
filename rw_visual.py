57be15306294ae12515bd61642c0689a8847857c
gianmillare <gianmillaregsm@gmail.com>
2020-03-17 22:14:31 -0700

    created another random walk that will display the beginning and ending points of the charts

--- rw_visual.py
+++ rw_visual.py
@@ -14,21 +14,68 @@ from random_walk import RandomWalk
 #
 # plt.show()
 
-# Part 2: Generating Multiple Random Walks - as long as the program is active
 
+
+# # Part 2: Generating Multiple Random Walks - as long as the program is active
+# while True:
+#     rw = RandomWalk()
+#     rw.fill_walk()
+#
+#     plt.style.use('seaborn')
+#     fig, ax = plt.subplots()
+#     ax.scatter(rw.x_values, rw.y_values, s=15)
+#
+#     plt.savefig("plots/rw2.png", dpi=100)
+#
+#     plt.show()
+#
+#     keep_running = input("Shall we make another walk? (y/n): ")
+#     if keep_running == 'n':
+#         break
+
+
+
+
+# # Part 3: Styling the Walk
+#
+# while True:
+#     rw = RandomWalk()
+#     rw.fill_walk()
+#
+#     plt.style.use('seaborn')
+#     fig, ax = plt.subplots()
+#     point_numbers = range(rw.num_points)
+#     ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+#     # edgecolors-'none' removes the black outline to make the colors more noticable
+#
+#     plt.savefig("plots/rw3.png", dpi=100)
+#
+#     plt.show()
+#
+#     keep_running = input('Shall we go for another walk? (y/n): ')
+#     if keep_running == 'n':
+#         break
+
+
+# Part 4: Plotting the Starting and Ending points
 while True:
     rw = RandomWalk()
     rw.fill_walk()
 
     plt.style.use('seaborn')
     fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values, s=15)
+    point_numbers = range(rw.num_points)
 
-    plt.savefig("plots/rw2.png", dpi=100)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+
+    # Here we begin emphasizing the beginning and ending points
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=175)
+
+    plt.savefig("plots/rw4.png", dpi=175)
 
     plt.show()
 
-    keep_running = input("Shall we make another walk? (y/n): ")
+    keep_running = input("Shall we go for another walk? (y/n): ")
     if keep_running == 'n':
         break
-
