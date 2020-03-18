00f158f3a42e4757ac29b7b1b9a1d37464a1486a
gianmillare <gianmillaregsm@gmail.com>
2020-03-17 20:11:17 -0700

    created the folder to save all the finalized plots and progression. completed code to create a scatter plot using two lists

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,1 +1,38 @@
 import matplotlib.pyplot as plt
+
+# # Part 1: plotting a single point
+# plt.style.use('seaborn')
+# fig, ax = plt.subplots()
+# ax.scatter(2,4)
+#
+# plt.show()
+
+# # Part 2: Styling a plot with a single point
+# plt.style.use('seaborn')
+# fig, ax = plt.subplots()
+# ax.scatter(2, 4, s=200)
+#
+# ax.set_title("Square Numbers", fontsize=24)
+# ax.set_xlabel("Value", fontsize=14)
+# ax.set_ylabel("Square of Value", fontsize=14)
+#
+# ax.tick_params(axis='both', which='major', labelsize=14)
+#
+# plt.show()
+
+
+# Part 3: Plotting a series of points
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.scatter(x_values, y_values, s=100)
+
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+ax.tick_params(axis='both', which='major', labelsize=14)
+
+plt.show()
