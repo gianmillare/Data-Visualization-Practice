6400aa022f32a0a17cfe894e746ac035379a2981
gianmillare <gianmillaregsm@gmail.com>
2020-03-17 19:53:38 -0700

    completed the basics of mlp_squares.py and created scatter_squares.py to practice scatter plots

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,9 +1,12 @@
 import matplotlib.pyplot as plt # Main import
 
+input_values = [1,2,3,4,5]
 squares = [1, 4, 9, 16, 25] # You will need a variable assigned to a list of values
 
+plt.style.use('seaborn')
 fig, ax = plt.subplots() # "fig" represents the figure and collection of plots, "ax" represents a single plot in the figure
-ax.plot(squares, linewidth = 3) # controls the width of the line
+ax.plot(input_values, squares, linewidth=3)
+# ax.plot(squares, linewidth = 3) # controls the width of the line
 
 # Chart Title and Axis
 ax.set_title("Square Numbers", fontsize=24) # Sets the title of the chart, also adds font size
