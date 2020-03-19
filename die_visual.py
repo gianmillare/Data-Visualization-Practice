ffb30c263ac88136378891a94c7a559d523f61af
gianmillare <gianmillaregsm@gmail.com>
2020-03-18 19:53:37 -0700

    created a way to record the frequency each side is rolled as a result

--- die_visual.py
+++ die_visual.py
@@ -1,12 +1,29 @@
 from die import Die
 
-# Part 1: Creating a six sided Die (D6)
+# # Part 1: Creating a six sided Die (D6)
+# die = Die()
+#
+# results = [] # Create an empty list
+# for roll_num in range(100): # We roll the Die 100 times
+#     result = die.roll() # we call the die.py file and the roll function within
+#     results.append(result) # call the variable 'results' and append the resulting roll
+#
+# print(results)
+
+
+
+# Part 2: Analyzing results: Counting Frequency
 die = Die()
 
-results = [] # Create an empty list
-for roll_num in range(100): # We roll the Die 100 times
-    result = die.roll() # we call the die.py file and the roll function within
-    results.append(result) # create another variable called 'results' and append the resulting roll
+results = []
+for roll_num in range(1000):
+    result = die.roll()
+    results.append(result)
 
-print(results)
+# Recording the frequency of each roll
+frequencies = [] # make an empty list
+for value in range (1, die.num_sides+1): # we add 1 (+1) so it will include the very last side
+    frequency = results.count(value) # count how many time a certain value appears
+    frequencies.append(frequency)
 
+print(frequencies)
