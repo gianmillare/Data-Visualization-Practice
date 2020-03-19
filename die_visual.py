536c6923571652b7a71de676dbcb344ac8595609
gianmillare <gianmillaregsm@gmail.com>
2020-03-18 20:12:27 -0700

    created the directory for plotly htmls, created a plotly plot that opens in a browser, and shows a histogram of frequency of results from rolling D6

--- die_visual.py
+++ die_visual.py
@@ -1,3 +1,6 @@
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+
 from die import Die
 
 # # Part 1: Creating a six sided Die (D6)
@@ -12,18 +15,42 @@ from die import Die
 
 
 
-# Part 2: Analyzing results: Counting Frequency
-die = Die()
+# # Part 2: Analyzing results: Counting Frequency
+# die = Die()
+#
+# results = []
+# for roll_num in range(1000):
+#     result = die.roll()
+#     results.append(result)
+#
+# # Recording the frequency of each roll
+# frequencies = [] # make an empty list
+# for value in range (1, die.num_sides+1): # we add 1 (+1) so it will include the very last side
+#     frequency = results.count(value) # count how many time a certain value appears
+#     frequencies.append(frequency)
+#
+# print(frequencies)
+
+
+
+#  Part 3: Make a Histogram
+die =Die()
 
 results = []
 for roll_num in range(1000):
     result = die.roll()
     results.append(result)
 
-# Recording the frequency of each roll
-frequencies = [] # make an empty list
-for value in range (1, die.num_sides+1): # we add 1 (+1) so it will include the very last side
-    frequency = results.count(value) # count how many time a certain value appears
+frequencies = []
+for value in range(1, die.num_sides + 1):
+    frequency = results.count(value)
     frequencies.append(frequency)
 
-print(frequencies)
+# Visualize the results
+x_values = list(range(1, die.num_sides + 1)) # x-axis will be the number of sides + 1
+data = [Bar(x=x_values, y=frequencies)]
+
+x_axis_config = {'title': 'Result'}
+y_axis_config = {'title': 'Frequency of Result'}
+my_layout = Layout(title='Results of rolling one D6 1000 times', xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='plotly_htmls/d6.html')
